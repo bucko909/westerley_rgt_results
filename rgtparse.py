4cb9dffe0f6865909f5a963b2abd40a44caeb51d
David Buckley <davidbuckley@gambitresearch.com>
2021-07-17 19:06:01 +0100

    add results for Westerley league

--- rgtparse.py
+++ rgtparse.py
@@ -26,9 +26,18 @@ def get_teams():
         teams[row[0]] = {'name': row[1], 'after': int(row[2]) if row[2] else 1, 'before': int(row[3]) if row[3] else float('inf')}
     return teams
 
+def get_westerley():
+    members = {}
+    csvfile = open(f'data/westerley.csv', 'r', newline='')
+    csvreader = csv.reader(csvfile)
+    for url, name, after in csvreader:
+        members[url] = int(after) if after else 0
+    return members
+
 def update_events():
     users = {}
     teams = get_teams()
+    westerley = get_westerley()
     team_points = {}
     if not os.path.exists('out'):
         os.mkdir('out')
@@ -45,8 +54,8 @@ def update_events():
             open(f'cache/{race_id}.html', 'w').write(html)
         csvfile = open(f'out/{race_no:02n}_{race_id}.csv', 'w')
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['pos', 'userurl', 'name', 'rgt_teamname', 'vr_teamname', 'team_qualifier', 'time_secs', 'delta_secs', 'wkg'])
-        for row in parse_event(html, teams, race_no):
+        csvwriter.writerow(['pos', 'userurl', 'name', 'rgt_teamname', 'vr_teamname', 'team_qualifier', 'westerley_pos', 'time_secs', 'delta_secs', 'wkg'])
+        for row in parse_event(html, teams, westerley, race_no):
             csvwriter.writerow(row)
             ingest_row(row, users, team_points)
             vr_teamname, team_qualifier = row[4:6]
@@ -60,19 +69,32 @@ def update_events():
         #    if points != team_points.get(team_name):
         #        print(team_name, points, team_points.get(team_name), team_points.get(team_name,0) - points)
         write_users(users, f'out/{race_no:02n}_{race_id}_users_cumulative.csv')
+        write_westerley(users, f'out/{race_no:02n}_{race_id}_westerley_cumulative.csv')
         write_teams(team_points, f'out/{race_no:02n}_{race_id}_teams_cumulative.csv')
 
     write_users(users)
+    write_westerley(users)
     write_teams(team_points)
 
 def write_users(users, fname='out/user_results.csv'):
     csvfile = open(fname, 'w')
     csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(['pos', 'url', 'name', 'points', 'team', 'team points', 'best', 'race count', 'win count', '2nd count', '3rd count'])
+    csvwriter.writerow(['pos', 'url', 'name', 'points', 'team', 'team points', 'westerley points', 'best', 'race count', 'win count', '2nd count', '3rd count'])
     users = list(users.items())
     users.sort(key=lambda u: u[1]['points'], reverse=True)
     for pos, u in enumerate(users, start=1):
-        csvwriter.writerow([pos, 'https://rgtdb.com' + u[0], u[1]['name'], u[1]['points'], u[1]['team'], u[1]['team_points'], u[1]['best'], u[1]['races'], u[1]['golds'], u[1]['silvers'], u[1]['bronzes']])
+        csvwriter.writerow([pos, 'https://rgtdb.com' + u[0], u[1]['name'], u[1]['points'], u[1]['team'], u[1]['team_points'], u[1]['westerley_points'], u[1]['best'], u[1]['races'], u[1]['golds'], u[1]['silvers'], u[1]['bronzes']])
+
+def write_westerley(users, fname='out/westerley_results.csv'):
+    csvfile = open(fname, 'w')
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['pos', 'url', 'name', 'westerley points', 'race count'])
+    users = list(users.items())
+    users.sort(key=lambda u: u[1]['westerley_points'], reverse=True)
+    for pos, u in enumerate(users, start=1):
+        if u[1]['westerley_points'] == 0:
+            continue
+        csvwriter.writerow([pos, 'https://rgtdb.com' + u[0], u[1]['name'], u[1]['westerley_points'], u[1]['races']])
 
 def write_teams(teams, fname='out/team_results.csv'):
     csvfile = open(fname, 'w')
@@ -83,9 +105,10 @@ def write_teams(teams, fname='out/team_results.csv'):
     for pos, t in enumerate(teams, start=1):
         csvwriter.writerow([pos, t[0], t[1]])
 
-def parse_event(html, teams, race_no):
+def parse_event(html, teams, westerley, race_no):
     data = lxml.etree.HTML(html)
     vr_team_runners = dict()
+    westerley_seen = set()
     for result in data.xpath('/html/body/div/main/div/div/div[@id="results"]/table/tbody/tr'):
         postd, _trophytd, userdatatd, timetd, wkgtd, _rankchangetd = result.xpath('td')
         dnfspan = postd.xpath('span')
@@ -140,14 +163,19 @@ def parse_event(html, teams, race_no):
         else:
             vr_teamname = None
             team_qualifier = None
-        yield (pos, userurl, username, rgt_teamname, vr_teamname, team_qualifier, time, delta, wkg)
+        if westerley.get(userurl, float('inf')) <= race_no:
+            westerley_seen.add(userurl)
+            westerley_pos = len(westerley_seen)
+        else:
+            westerley_pos = None
+        yield (pos, userurl, username, rgt_teamname, vr_teamname, team_qualifier, westerley_pos, time, delta, wkg)
 
 def ingest_row(row, users, team_points):
-    pos, userurl, username, rgt_teamname, vr_teamname, team_qualifier, time, delta, wkg = row
+    pos, userurl, username, rgt_teamname, vr_teamname, team_qualifier, westerley_pos, time, delta, wkg = row
     if pos:
         if userurl is None:
             raise Exception(lxml.html.tostring(result))
-        users.setdefault(userurl, {'name': username, 'points': 0, 'team_points': 0, 'races': 0, 'golds': 0, 'silvers': 0, 'bronzes': 0, 'team': vr_teamname})
+        users.setdefault(userurl, {'name': username, 'points': 0, 'team_points': 0, 'westerley_points': 0, 'races': 0, 'golds': 0, 'silvers': 0, 'bronzes': 0, 'team': vr_teamname})
         user = users[userurl]
         if user['team'] is None or vr_teamname is None:
             # allow flips between None
@@ -168,6 +196,8 @@ def ingest_row(row, users, team_points):
             team_points.setdefault(vr_teamname, 0)
             team_points[vr_teamname] += race_points
             user['team_points'] += race_points
+        if westerley_pos is not None:
+            user['westerley_points'] += max(1, 21 - westerley_pos)
 
 if __name__ == '__main__':
     update_events()
